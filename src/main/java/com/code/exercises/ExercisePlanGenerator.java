59359d03122f2d5c070f924b56d91c5d377cc6ab
miguel.pimenta <pimenta.mig@gmail.com>
2022-08-30 23:11:28 +0100

    Change message to verify GHA deployment

--- src/main/java/com/code/exercises/ExercisePlanGenerator.java
+++ src/main/java/com/code/exercises/ExercisePlanGenerator.java
@@ -4,7 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 
 public class ExercisePlanGenerator {
     public String handleRequest(String input, Context context) {
-        context.getLogger().log("Input xxx: " + input);
-        return "Hello World xxx - " + input;
+        context.getLogger().log("Input yyy: " + input);
+        return "Hello World yyy - " + input;
     }
 }
